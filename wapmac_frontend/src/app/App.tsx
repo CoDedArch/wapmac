ba8a4ba745705056158f05b307aa42a808c2634a
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2025-02-24 08:51:55 +0000

    ensure that each endpoint scrolls to the top

--- wapmac_frontend/src/app/App.tsx
+++ wapmac_frontend/src/app/App.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../ui/homePage";
+import ScrollToTop from "@/shared/presentation/scrollToTop";
 import LearnMorePage from "@/ui/learnMorePage";
 
 function App() {
-
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/learn-more" element={<LearnMorePage />} />
       </Routes>
     </Router>
-   
-  )
+  );
 }
 
-export default App
+export default App;
